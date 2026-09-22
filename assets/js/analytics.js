dfb85f9743ed8496aa24ffb4f05f187f473682e7
jalaldhalipM <jalaldhalipM@users.noreply.github.com>
2026-09-22 18:55:45 +0530

    Add Google tag script to all page head sections

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,7 +1,7 @@
-// Optional GA4: add the Google tag loader in each page head and replace this placeholder.
+// Google tag setup
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
 gtag("js", new Date());
-gtag("config", "G-XXXXXXXXXX");
+gtag("config", "AW-18336418868");
